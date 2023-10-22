4296b4cf4e82ec07b24d88f6af04c39705cc3bbc
Rodrigo <rodrigo.amaya@utec.edu.pe>
2023-10-21 21:52:30 -0500

    añadido de parte 2, faltante parte 7 (pregunta 1)

--- pregunta1.py
+++ pregunta1.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
 
@@ -8,15 +7,20 @@ print(dataset)
 
 km = KMeans(18, n_init=10)
 
-vec = TfidfVectorizer(ngram_range=(2,3))
+vec = TfidfVectorizer(ngram_range=(2, 3))
 x = vec.fit_transform(dataset["moves"])
 z = sorted(vec.vocabulary_.keys())
-tf = pd.DataFrame(data = x.toarray(), columns=z)
+tf = pd.DataFrame(data=x.toarray(), columns=z)
 
 km = KMeans(n_clusters=18, n_init=10)
 kmlista = km.fit_predict(tf)
 tf["Tipo"] = kmlista
 print(vec.vocabulary_)
 print(kmlista)
+
+# Mostrar el número total de tokens (elementos de su vocabulario)
+num_tokens = len(vec.vocabulary_)
+print("\nNumero total de tokens en la matriz:", num_tokens,"\n")
+
 print(tf)
 tf.to_csv("Smogon_agrupados.csv")
